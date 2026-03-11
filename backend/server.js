ffc41f1810be9e44be61dd73fce0e8fbe9d56ebf
debracarino <dcarino@clackamas.edu>
2026-03-11 09:56:39 -0700

    Updated PORT

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ require("./config/db");
 const User = require('./models/User');
 const userRouter = require('./routes/userRouter');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
